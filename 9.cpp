affbdd0e41e999f6ecb9499bf1fc25919d1abc36
_Yuu <91846346+ahmedelia@users.noreply.github.com>
2022-05-21 03:24:28 +0200

    Create 9.cpp

---
+++ 9.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    //مش فاهم السؤال اوي بصراحه 
+    float p,r,n,v;cin>>p>>r>>n;
+    cout<<"p\tr\tn\tv"<<endl;
+    cout<<p<<'\t'<<r<<'\t'<<n<<'\t'<<p * pow(1+r,n)<<endl;
+    return 0;
+}
