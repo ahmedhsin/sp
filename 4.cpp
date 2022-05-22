62b623f90a4fed646579a60f288ff02e7204dae9
AMNA <74130553+nono5342@users.noreply.github.com>
2022-05-22 12:20:17 +0200

    عدلت ف شرط التوقف والloop
    (if N has negative value stop
    asking the user)
    الكود كان مكتوب على أنه يوقف عند -1 بس وبيضرب لو دخل عدد سالب غيره ف عدلته
    وكمان شلت الif condition الي قبل الloop

--- 4.cpp
+++ 4.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 /*
 انا حاسس في مشكله فالسؤال عشان مينفعش
@@ -13,19 +12,17 @@ using namespace std;
 *
 
 */
-main()
+int main()
 {
     int n;
     do{
         cout<<"enter number : ";
         cin>>n;
-        if(n != -1){
-        for(int i = n;i!=0;i--){
+        for(int i = n;i > 0;i--){
             for(int j = 1;j<=i;j++)
                 cout<<'*';
         cout<<endl;
         }
-        }
-    }while(n != -1);
+    }while(n >= 0);
     return 0;
 }
